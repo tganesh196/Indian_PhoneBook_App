2db909a343d7d4eb9ddb2b92d4a856be7d1bc017
tganesh169 <tganesh169@gmail.com>
2022-08-18 20:51:17 +0530

    IPA -1 : project created

---
+++ src/main/java/com/phone/IndianPhoneBookAppApplication.java
@@ -0,0 +1,13 @@
+package com.phone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndianPhoneBookAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndianPhoneBookAppApplication.class, args);
+	}
+
+}
